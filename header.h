217d69e162ea70d8a8ab13862e1e5d617b9e7166
Siddesh G M <siddesh.gm25@gmail.com>
2026-04-09 22:08:24 +0530

    Man Page Updated And my_ps command Updated

--- header.h
+++ header.h
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
@@ -9,8 +10,9 @@
 #include<sys/stat.h>
 #include<dirent.h>
 #include<time.h>
+#include <fcntl.h>              /* Definition of O_* constants */
+#include <unistd.h>
 void logs_file(char** );
 void list(char *,char *);
-void print(int);
 
 
