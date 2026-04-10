40cb7506fc3d79af15b624e9f6c21e70e89d56c5
Siddesh G M <siddesh.gm25@gmail.com>
2026-04-10 09:24:22 +0530

    Added Path Display Feature and cd Command

--- main.c
+++ main.c
@@ -5,8 +5,23 @@ int main(){
 	char* args[100];
 	while(1){
 		if(flag){
-			printf("myshell>");
-			scanf(" %[^\n]",cmd);
+			char cwd[1024];
+			char *user = getenv("USER");
+
+			getcwd(cwd, sizeof(cwd));
+
+			char home[1024];
+			snprintf(home, sizeof(home), "/home/%s", user);
+
+			if (strncmp(cwd, home, strlen(home)) == 0){
+				printf("\033[1;32m%s@myshell\033[0m:\033[1;34m~%s\033[0m$ ",user, cwd + strlen(home));
+			} 
+			else{
+				printf("\033[1;32m%s@myshell\033[0m:\033[1;34m%s\033[0m$ ",user, cwd);
+			}
+			fgets(cmd,1024, stdin);
+			cmd[strcspn(cmd, "\n")] = 0;
+			memset(args, 0, sizeof(args));
 			flag=0;
 			int i=0;
 			char* token=strtok(cmd," ");
@@ -19,6 +34,27 @@ int main(){
 		if(args[0]==NULL){
 			continue;
 		}
+		if(strcmp(args[0], "cd") == 0){
+			if(args[1] == NULL){
+				if(chdir(getenv("HOME")) != 0)
+					perror("cd");
+			}
+			else{
+				if(chdir(args[1]) != 0)
+					perror("cd");
+			}
+			flag=1;
+			continue;
+		}
+		/*if(strcmp(args[0],"cd")==0){
+			if(args[1]==NULL){
+				chdir(getenv("HOME"));
+			}
+			else{
+				chdir(args[1]);
+			}
+			continue;
+		}*/
 		logs_file(args);
 		if(strcmp(args[0],"exit")==0){
 			printf("exiting shell...\n");
